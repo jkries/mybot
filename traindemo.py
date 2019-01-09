17e0c2a14a146cedc44b22bd38a7fc48f1b97016
Jason Kries <jkries@gmail.com>
2019-01-09 09:25:54 -0500

    Add files via upload

--- traindemo.py
+++ traindemo.py
@@ -11,22 +11,21 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 lineCount = 0
-with open('data/trainingdata.yml', 'w') as f:
+with open('data/demodata.yml', 'w') as f:
     f.write("categories:\r\n")
     f.write("- Conversations")
     f.write("\r\nconversations:")
-    with open('data/chatbot.csv') as g:
+    with open('testbot.csv') as g:
         lines = csv.reader(g)
         for line in lines:
             lineCount += 1
-            if lineCount >= 1:
-                f.write("\r\n- - " + line[0])
-                f.write("\r\n  - " + line[1])
+            f.write("\r\n- - " + line[0])
+            f.write("\r\n  - " + line[1])
 
 print("I have successfully imported " + str(lineCount) + " rows of info and will now retrain...")
 
-if os.path.exists("botData.sqlite3"):
-    os.remove("botData.sqlite3")
+if os.path.exists("demoData.sqlite3"):
+    os.remove("demoData.sqlite3")
     print("Clearing my old training data.")
 
 bot = ChatBot(
@@ -45,7 +44,7 @@ bot = ChatBot(
     input_adapter="chatterbot.input.VariableInputTypeAdapter",
     output_adapter="chatterbot.output.OutputAdapter",
     storage_adapter="chatterbot.storage.SQLStorageAdapter",
-    database="botData.sqlite3"
+    database="demoData.sqlite3"
 )
 
 bot.read_only=True #Comment this out if you want the bot to learn based on experience
@@ -53,6 +52,6 @@ print("Bot Learn Read Only:" + str(bot.read_only))
 
 #You can comment these out for production later since you won't be training everytime:
 bot.set_trainer(ChatterBotCorpusTrainer)
-bot.train("data/trainingdata.yml")
+bot.train("data/demodata.yml")
 
 print("I am all trained up and ready to chat!")

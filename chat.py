42cfc4964d5c06dc37b212df319d9b664780cfb9
Jason Kries <jkries@gmail.com>
2019-01-09 09:24:51 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,15 +5,12 @@ import random
 #from chatterbot.trainers import ChatterBotCorpusTrainer
 
 ##Experimental Date Time
-from dateTime import getTime, getDate 
-
-#Google it:
-#from googleNow import tryGoogle
+from dateTime import getTime, getDate
 
 import logging
 logging.basicConfig(level=logging.INFO)
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 bot = ChatBot(
     "ChatBot",
@@ -37,23 +34,26 @@ bot = ChatBot(
 bot.read_only=True #Comment this out if you want the bot to learn based on experience
 print("Bot Learn Read Only:" + str(bot.read_only))
 
-
 #You can comment these out for production later since you won't be training everytime:
 #bot.set_trainer(ChatterBotCorpusTrainer)
 #bot.train("data/trainingdata.yml")
 
-@app.route("/")
+def tryGoogle(myQuery):
+	#print("<br>Try this from my friend Google: <a target='_blank' href='" + j + "'>" + query + "</a>")
+	return "<br>You can try this from my friend Google: <a target='_blank' href='https://www.google.com/search?q=" + myQuery + "'>" + myQuery + "</a>"
+
+@application.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/get")
+@application.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
     botReply = str(bot.get_response(userText))
-    if botReply == "IDKnull":
+    if botReply is "IDKnull":
         # Create a list of non-responses:
-        noResponse = ["I don't know.", "I'm not sure about that.", "Is there a different way you can ask that?","I don't have a response for that.","I will have to give that some thought.","I don't really know what you are asking."]
-        botReply = random.choice(noResponse)
+        noResponse = ["I don't know.", "I'll need to take some time to think on that.", "Can you try rephrasing that please?", "I'm not sure about that.", "Is there a different way you can ask that?","I don't have a response for that.","I will have to give that some thought.","I don't really know what you are asking."]
+        botReply = random.choice(noResponse) + tryGoogle(userText)
     elif botReply == "getTIME":
         botReply = getTime()
         print(getTime())
@@ -62,6 +62,7 @@ def get_bot_response():
         print(getDate())
     return botReply
 
+
 if __name__ == "__main__":
-    #app.run()
-    app.run(host='0.0.0.0', port=80)
+    #application.run()
+    application.run(host='0.0.0.0', port=80)

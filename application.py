c9c2d8760e45b816d4d3ea98d6db9ab54f0501e6
Jason Kries <jkries@gmail.com>
2019-01-15 22:20:34 -0500

    File for AWS Elastic Beanstalk

---
+++ application.py
@@ -0,0 +1,72 @@
+#! /usr/bin/python3
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.response_selection import get_random_response
+import random
+#from chatterbot.trainers import ChatterBotCorpusTrainer
+from botConfig import myBotName, chatBG
+
+##Experimental Date Time
+from dateTime import getTime, getDate
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+application = Flask(__name__)
+
+chatbotName = myBotName
+print("Bot Name set to: " + chatbotName)
+
+bot = ChatBot(
+    "ChatBot",
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch'
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.65,
+            'default_response': 'IDKresponse'
+        }
+    ],
+    response_selection_method=get_random_response, #Comment this out if you want best response
+    input_adapter="chatterbot.input.VariableInputTypeAdapter",
+    output_adapter="chatterbot.output.OutputAdapter",
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    database="botData.sqlite3"
+)
+
+bot.read_only=True #Comment this out if you want the bot to learn based on experience
+print("Bot Learn Read Only:" + str(bot.read_only))
+
+#You can comment these out for production later since you won't be training everytime:
+#bot.set_trainer(ChatterBotCorpusTrainer)
+#bot.train("data/trainingdata.yml")
+
+def tryGoogle(myQuery):
+	#print("<br>Try this from my friend Google: <a target='_blank' href='" + j + "'>" + query + "</a>")
+	return "<br><br>You can try this from my friend Google: <a target='_blank' href='https://www.google.com/search?q=" + myQuery + "'>" + myQuery + "</a>"
+
+@application.route("/")
+def home():
+    return render_template("index.html", botName = chatbotName, chatBG = chatBG)
+
+@application.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    botReply = str(bot.get_response(userText))
+    if botReply is "IDKresponse":
+        botReply = str(bot.get_response('IDKnull')) ##Send the i don't know code back to the DB
+        botReply = botReply + tryGoogle(userText)
+    elif botReply == "getTIME":
+        botReply = getTime()
+        print(getTime())
+    elif botReply == "getDATE":
+        botReply = getDate()
+        print(getDate())
+    return botReply
+
+
+if __name__ == "__main__":
+    application.run()
+    #application.run(host='0.0.0.0', port=80)

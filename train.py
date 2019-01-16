45abea2f2e6be4b0d60f7a72095affc69b521add
Jason Kries <jkries@gmail.com>
2019-01-16 09:07:07 -0500

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,6 @@
 
 #from flask import Flask, render_template, request
 from chatterbot import ChatBot
-from chatterbot.response_selection import get_random_response
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 #For getting the spreadsheet data from csv
@@ -33,25 +32,9 @@ if os.path.exists("botData.sqlite3"):
 
 bot = ChatBot(
     "Chat Bot",
-    logic_adapters=[
-        {
-            'import_path': 'chatterbot.logic.BestMatch'
-        },
-        {
-            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
-            'threshold': 0.65,
-            'default_response': 'I don\'t have a response for that. What else can we talk about?'
-        }
-    ],
-    response_selection_method=get_random_response, #Comment this out if you want best response
-    input_adapter="chatterbot.input.VariableInputTypeAdapter",
-    output_adapter="chatterbot.output.OutputAdapter",
     storage_adapter="chatterbot.storage.SQLStorageAdapter",
     database="botData.sqlite3"
 )
-
-bot.read_only=True #Comment this out if you want the bot to learn based on experience
-print("Bot Learn Read Only:" + str(bot.read_only))
 
 #You can comment these out for production later since you won't be training everytime:
 bot.set_trainer(ChatterBotCorpusTrainer)

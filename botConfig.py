4b9c1ae84dc86ea37f0520a658e02752d99d6cce
Jason Kries <jkries@gmail.com>
2019-01-20 22:29:42 -0500

    Update botConfig.py

--- botConfig.py
+++ botConfig.py
@@ -5,6 +5,7 @@ myBotName = "Marvin the ChatBot"
 botTimeZone = "America/New_York"  ##See the list full below
 botAvatar = "/static/bot.png" ##This can either be an image in your static folder or a web url to an image
 chatBG = "/static/bg.jpg"  ##This can either be an image in your static folder or a web url to an image
+useGoogle = "yes" ## yes or no if you want Google links provided when your Bot does not have an answer
 
 ##List of timezones:
 '''

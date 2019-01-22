10840f643173ac6d35554ec51f1a21e17196c3b8
Jason Kries <jkries@gmail.com>
2019-01-22 10:06:06 -0500

    Update botConfig.py

--- botConfig.py
+++ botConfig.py
@@ -6,6 +6,8 @@ botTimeZone = "America/New_York"  ##See the list full below
 botAvatar = "/static/bot.png" ##This can either be an image in your static folder or a web url to an image
 chatBG = "/static/bg.jpg"  ##This can either be an image in your static folder or a web url to an image
 useGoogle = "yes" ## yes or no if you want Google links provided when your Bot does not have an answer
+confidenceLevel = 0.65 ##Bot confidence level - must be betwee. 0.00 and 1, default is 0.65
+
 
 ##List of timezones:
 '''

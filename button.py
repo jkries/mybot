521d03cf99d1f7a34f962001db403786fa1df20d
Jason Kries <jkries@gmail.com>
2019-06-09 20:42:22 -0400

    Add files via upload

--- button.py
+++ button.py
@@ -1,10 +1,22 @@
+import pyperclip
+
 while True:
     print('What do you want the button to say:')
-    linkText = input('Button Text: ').replace(r"'", r"\'")
+    linkText = input('Button Text: ')
 
-    newLink = '<a class="chatSuggest" onclick="$(this).hide(); chatSuggest(\'' + linkText + '\')";>' + linkText + '</a>'
+    print('What do you want to send to the ChatBot (leave blank to send button text):')
+    queryText = input('Query Text: ').replace(r"'", r"\'")
+
+    if queryText == "":
+        queryText = linkText.replace(r"'", r"\'")
+
+    newLink = '<a class="chatSuggest" onclick="$(this).hide(); chatSuggest(\'' + queryText + '\')";>' + linkText + '</a><br>'
+    pyperclip.copy(newLink)
     print()
-    print('==========================')
+    print()
+    print('====================================================')
     print('Just copy and paste this into your knowledge base response:')
     print(newLink)
-    print('==========================')
+    print('====================================================')
+    print()
+    print()

75bb49851e450313958514ebfcfe66e2d8499ecb
Jason Kries <jkries@gmail.com>
2021-12-28 00:23:02 -0500

    Add files via upload
    Added better matching for exact matches

--- botRespond.py
+++ botRespond.py
@@ -11,7 +11,10 @@ def getResponse(sendMsg):
     #Loop through CSV knowledge file.  If a question is equal to or greater than the confidence level, add it to a list of possible responses. Then return a random responses
     lineCount = 0
     successCount = 0
+    exactCount = 0
     comeBacks = []
+    exactReply = []
+    exactMatch = .9
     with open('data/chatbot.csv') as g:
         lines = csv.reader(g)
         for line in lines:
@@ -23,12 +26,19 @@ def getResponse(sendMsg):
                 userText = line[0]
                 botReply = line[1]
                 checkMatch = similar(sendMsg, userText)
-                if checkMatch >= confidenceLevel:
+                if checkMatch >= exactMatch:
+                    exactCount += 1
+                    exactReply.append(botReply)
+                    print("Likely match: " + userText)
+                    print("Match is: " + str(checkMatch))
+                elif checkMatch >= confidenceLevel:
                     successCount += 1
                     comeBacks.append(botReply)
-                #Better than the confidenceLevel?
-                #print("Match is: " + str(checkMatch))
-    if successCount >= 1:
+                    print("Possible match: " + userText)
+                    print("Match is: " + str(checkMatch))
+    if exactCount >= 1:
+        botResponsePick = random.choice(exactReply)
+    elif successCount >= 1:
         botResponsePick = random.choice(comeBacks)
     else:
         botResponsePick = "IDKresponse"

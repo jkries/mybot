0cf41db0791e02dcec1ee8672b1b9c4367823f50
Jason Kries <jkries@gmail.com>
2023-04-10 11:38:25 -0400

    Modified code for PythonAnywhere

---
+++ buttonPE.py
@@ -0,0 +1,19 @@
+while True:
+    print('What do you want the button to say:')
+    linkText = input('Button Text: ')
+
+    print('What do you want to send to the ChatBot (leave blank to send button text):')
+    queryText = input('Query Text: ').replace(r"'", r"\'")
+
+    if queryText == "":
+        queryText = linkText.replace(r"'", r"\'")
+
+    newLink = '<a class="chatSuggest" onclick="$(this).hide(); chatSuggest(\'' + queryText + '\')";>' + linkText + '</a><br>'
+    print()
+    print()
+    print('====================================================')
+    print('Button code created! Now copy and paste this into your knowledge base response:')
+    print(newLink)
+    print('====================================================')
+    print()
+    print()
